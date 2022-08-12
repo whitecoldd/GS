de490219d56f7a7398553df9f6e5d17bc1f549ea
whitecoldd <buyukliwc@gmail.com>
2022-08-12 22:08:06 +0300

    v1.2.0

--- GSDONE/src/components/topForm/TopForm.jsx
+++ GSDONE/src/components/topForm/TopForm.jsx
@@ -184,7 +184,7 @@ export default function TopForm({ Open, setOpen, Data, setData, currAmount, setc
             {errors.country && <p>Please fill in all fields</p>}
             {errors.email && <p>Please fill in all fields</p>}
             {errors.egift_number && <p>Please fill in all fields</p>}
-            {formValidated && <p>This e-gift number has already been used</p>}
+            {!formValidated && <p>This e-gift number has already been used</p>}
           </form>
         </div>
       </div>
