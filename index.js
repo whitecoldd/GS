205a9b4db132c65f36926ce9ef7a9c14e3f7e960
whitecoldd <buyukliwc@gmail.com>
2022-08-12 20:14:31 +0300

    v1.0.6

--- index.js
+++ index.js
@@ -39,18 +39,17 @@ app.use(cors());
 //     })
 //     return res.send()
 // })
-
-if (process.env.NODE_ENV === "production") {
-    app.use(express.static("GSDONE/build"));
-  
-    app.get("*", (req, res) => {
-      res.sendFile(path.resolve(__dirname, "GSDONE", "build", "index.html"));
-    });
-  }
-
 app.use(express.json());
 app.use("/api/form", formRoute);
 app.use("/api/table", tableRoute);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("GSDONE/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "GSDONE", "build", "index.html"));
+  });
+}
 
 app.listen(process.env.PORT || 5000, async () => {
   console.log("backend server is up on PORT " + process.env.PORT || 5000);
