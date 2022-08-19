d844c52b3f227b64f0bfb51a9c2274db19b78ef1
Vladimir <99729335+whitecoldd@users.noreply.github.com>
2022-08-19 14:38:49 +0300

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const formRoute = require("./routes/form");
 const bodyParser = require("body-parser");
 const axios = require("axios");
 const cors = require("cors");
-const path = require("path");
+
 dotenv.config();
 // const { TOKEN, SERVER_URL } = process.env
 // const TELEGRAM_API = `https://api.telegram.org/bot${TOKEN}`
@@ -21,7 +21,7 @@ mongoose
   });
 app.use(cors());
 
-// app.use(bodyParser.json())
+app.use(bodyParser.json())
 
 // const init = async () => {
 //     const res = await axios.get(`${TELEGRAM_API}/setWebhook?url=${WEBHOOK_URL}`)
@@ -40,19 +40,19 @@ app.use(cors());
 //     })
 //     return res.send()
 // })
-app.use(express.json());
+//app.use(express.json());
 app.use("/api/form", formRoute);
 app.use("/api/table", tableRoute);
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("GSDONE/build"));
-
+  const path = require("path");
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "GSDONE", "build", "index.html"));
   });
 }
 
-app.listen(process.env.PORT || 5000, async () => {
-  console.log("backend server is up on PORT " + process.env.PORT || 5000);
+app.listen(process.env.PORT || 9000, async () => {
+  console.log("backend server is up on PORT " + process.env.PORT || 9000);
   //await init()
 });
